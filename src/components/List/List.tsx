21f4da7f3b28c2a7d8b94a4aae63c458a7f3451a
František Pác <5476595+fandasson@users.noreply.github.com>
2024-02-01 20:34:58 +0100

    feat(style): style `List` component

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,3 +1,9 @@
+import { Country } from "components/List/Country";
+import { Flag } from "components/List/Flag";
+import { ListItem } from "components/List/ListItem";
+import { ListLayout } from "components/List/ListLayout";
+import { Rate } from "components/List/Rate";
+import { getFlagEmoji } from "components/List/list.helpers";
 import { ExchangeRate } from "store/api/types";
 
 type Props = {
@@ -12,22 +18,21 @@ const List = (props: Props) => {
     }
 
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Currency</th>
-                    <th>Rate</th>
-                </tr>
-            </thead>
-            <tbody>
-                {exchangeRates.map((exchangeRate) => (
-                    <tr key={exchangeRate.code}>
-                        <td>{exchangeRate.currency}</td>
-                        <td>{exchangeRate.rate}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <ListLayout>
+            {exchangeRates.map((exchangeRate) => (
+                <ListItem key={exchangeRate.code}>
+                    <Flag>{getFlagEmoji(exchangeRate.code.substring(0, 2))}</Flag>
+                    <Country>{exchangeRate.country}</Country>
+                    <Rate>
+                        <strong>{exchangeRate.rate} CZK</strong>
+                        <small>
+                            for {exchangeRate.amount}{" "}
+                            {exchangeRate.amount > 1 ? `${exchangeRate.currency}s` : exchangeRate.currency}
+                        </small>
+                    </Rate>
+                </ListItem>
+            ))}
+        </ListLayout>
     );
 };
 export { List };
---
+++ src/components/List/ListLayout.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ListLayout = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-radius: 0.5rem;
+    padding: 1rem 0;
+    background-color: var(--white);
+`;
