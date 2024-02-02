d4a4005e140c9810dca0eaa2b569fd4af802421c
František Pác <5476595+fandasson@users.noreply.github.com>
2024-02-02 12:35:35 +0100

    feat(style): style texts used in ExchangeRate
    A `Typography` component was introduced

--- src/components/ExchangeRate/ExchangeRate.tsx
+++ src/components/ExchangeRate/ExchangeRate.tsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 import { AmountInput } from "components/ExchangeRate/AmountInput";
 import { CurrencySelector } from "components/ExchangeRate/CurrencySelector";
 import { Loading } from "components/ui";
+import { Typography } from "components/ui/Typography";
 import { useExchangeRates } from "store/api/hooks";
 import { ExchangeRate } from "store/api/types";
 import { convertCurrency } from "utils/exchange.helpers";
 
+// FIXME: prevent re-rendering of CurrencySelector on state change
 const _ExchangeRate = () => {
     const { data, isPending } = useExchangeRates();
     const [currency, setCurrency] = useState<ExchangeRate | null>(null); // [1
@@ -18,11 +20,19 @@ const _ExchangeRate = () => {
 
     const result = currency && amount ? convertCurrency(currency, amount) : null;
     return (
-        <>
-            <AmountInput onChange={setAmount} />
-            <CurrencySelector currencies={data} onSelect={setCurrency} />
-            <div>Result: {result?.toFixed(2)}</div>
-        </>
+        <Typography.Centered>
+            <Typography.Prompt>
+                Exchange
+                <AmountInput onChange={setAmount} /> CZK
+            </Typography.Prompt>
+            <Typography.Small>for</Typography.Small>
+
+            <div>
+                {!currency && <Typography.Hint>Select currency first 👉</Typography.Hint>}
+                {currency && <Typography.Prompt>{result?.toFixed(2)}</Typography.Prompt>}
+                <CurrencySelector currencies={data} onSelect={setCurrency} />
+            </div>
+        </Typography.Centered>
     );
 };
 
