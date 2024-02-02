9d8821f845d06195ad3daff404dcfd408ce16c55
František Pác <5476595+fandasson@users.noreply.github.com>
2024-02-02 14:32:51 +0100

    feat(style): layout of components within ExchangeRate

--- src/components/ExchangeRate/ExchangeRate.tsx
+++ src/components/ExchangeRate/ExchangeRate.tsx
@@ -1,12 +1,28 @@
 import { useState } from "react";
+import styled from "styled-components";
 
 import { AmountInput } from "components/ExchangeRate/AmountInput";
 import { CurrencySelector } from "components/ExchangeRate/CurrencySelector";
+import { PromptLayout } from "components/ExchangeRate/PromptLayout";
 import { Loading } from "components/ui";
 import { Typography } from "components/ui/Typography";
+import { ExchangeIcon } from "components/ui/icons";
 import { useExchangeRates } from "store/api/hooks";
 import { ExchangeRate } from "store/api/types";
 import { convertCurrency } from "utils/exchange.helpers";
+
+const Wrapper = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    gap: 1.4rem;
+    margin-bottom: 2rem;
+
+    small {
+        display: block;
+        margin: 0.5rem 0;
+    }
+`;
 
 // FIXME: prevent re-rendering of CurrencySelector on state change
 const _ExchangeRate = () => {
@@ -20,19 +36,22 @@ const _ExchangeRate = () => {
 
     const result = currency && amount ? convertCurrency(currency, amount) : null;
     return (
-        <Typography.Centered>
-            <Typography.Prompt>
-                Exchange
-                <AmountInput onChange={setAmount} /> CZK
-            </Typography.Prompt>
-            <Typography.Small>for</Typography.Small>
-
-            <div>
-                {!currency && <Typography.Hint>Select currency first 👉</Typography.Hint>}
-                {currency && <Typography.Prompt>{result?.toFixed(2)}</Typography.Prompt>}
-                <CurrencySelector currencies={data} onSelect={setCurrency} />
-            </div>
-        </Typography.Centered>
+        <Wrapper>
+            <ExchangeIcon />
+            <Typography.Centered>
+                <Typography.Prompt>
+                    <PromptLayout>
+                        <AmountInput onChange={setAmount} />
+                        <span>CZK</span>
+                    </PromptLayout>
+                </Typography.Prompt>
+                <Typography.Small>exchanges for</Typography.Small>
+                <PromptLayout>
+                    {currency && <Typography.Prompt>{result?.toFixed(2)}</Typography.Prompt>}
+                    <CurrencySelector currencies={data} onSelect={setCurrency} />
+                </PromptLayout>
+            </Typography.Centered>
+        </Wrapper>
     );
 };
 
